e8463e967cffa065b7e1d502db0389bea8da31ed
Matheus Araujo <matheus.silvano@hotmail.com>
2020-03-29 00:43:38 -0300

    Seeding Service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -28,15 +29,18 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                         builder.MigrationsAssembly("SalesWebMvc"))); //configuração do gerenciador de banco que vai ser usado na aplicação
+
+            services.AddScoped<SeedingService>(); //registra o serviço no sistema de injeção de dependencia da aplicação
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed(); //se estiver em ambiente de desenvolvimento vai popular a base, caso já não esteja populada
             }
             else
             {
